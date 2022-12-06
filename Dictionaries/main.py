e1d0b12cafcd93fdc9d43754aa756a5be1958f51
LatvianRaccoon <arturs.svikis@gmail.com>
2022-12-06 12:38:19 +0100

    Dictionaries Basics

---
+++ Dictionaries/main.py
@@ -0,0 +1,18 @@
+eng2sp = {}
+eng2sp['one'] = 'uno'
+eng2sp['two'] = 'dos'
+eng2sp['three'] = 'tres'
+print(eng2sp)
+
+value = eng2sp['two']
+print(value)
+print(eng2sp['one'])
+
+medals = {'gold': 33, 'silver': 17, 'bronze': 12}
+print(medals)
+
+olympics = {}
+olympics['gold'] = 7
+olympics['silver'] = 8
+olympics['bronze'] = 6
+print(olympics)
