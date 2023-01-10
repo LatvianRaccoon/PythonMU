915752f1b40187c1f501f76b91ad8089cbedeaa5
LatvianRaccoon <arturs.svikis@gmail.com>
2023-01-10 11:32:42 +0100

    Tuple packing exercises

--- Tuples/TuplePacking/main.py
+++ Tuples/TuplePacking/main.py
@@ -14,3 +14,22 @@ def circleInfo(r):
 
 
 print(circleInfo(10))
+
+
+lst_tups = [('Articuno', 'Moltres', 'Zaptos'), ('Beedrill', 'Metapod', 'Charizard', 'Venasaur', 'Squirtle'), ('Oddish', 'Poliwag', 'Diglett', 'Bellsprout'), ('Ponyta', "Farfetch'd", "Tauros", 'Dragonite'), ('Hoothoot', 'Chikorita', 'Lanturn', 'Flaaffy', 'Unown', 'Teddiursa', 'Phanpy'), ('Loudred', 'Volbeat', 'Wailord', 'Seviper', 'Sealeo')]
+
+t_check = []
+
+for tup in lst_tups:
+    t_check.append(tup[2])
+
+print(t_check)
+
+tups = [('a', 'b', 'c'), (8, 7, 6, 5), ('blue', 'green', 'yellow', 'orange', 'red'), (5.6, 9.99, 2.5, 8.2), ('squirrel', 'chipmunk')]
+
+seconds = []
+
+for tup in tups:
+    seconds.append(tup[1])
+
+print(seconds)
